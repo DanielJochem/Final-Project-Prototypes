e52566690010b0bca8418ec3a8e401bc7ad5bf6c
DanielJochem <daniel.jochem@gmail.com>
2016-10-08 23:49:35 +1000

    Procedural Generation Prototype

---
+++ Procedural Generation Prototype/Assets/CameraController2D.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController2D : MonoBehaviour {
+
+    public Transform target;
+
+    //The Distance and Height from the object.
+    public float distance = 20.0f;
+    public float height = 5.0f;
+
+    public Vector3 lookAtVector;
+
+    // Use this for initialization
+    void Start () {
+        Camera.main.orthographicSize = 10;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(target.position.x, target.position.y, -10);
+    }
+}
---
+++ Procedural Generation Prototype/Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	Rigidbody rb;
+	Vector3 velocity;
+    public float speed;
+    private Vector3 rot;
+	
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+        rot = rot;
+	}
+
+	void Update() {
+		velocity = new Vector3 (0, 0, Input.GetAxisRaw ("Vertical")).normalized * 10;
+	}
+
+	void FixedUpdate() {
+        //rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        //transform.position = (transform.position + velocity) * Time.fixedDeltaTime;
+        transform.Translate(velocity * Time.fixedDeltaTime);
+
+        //if(rot.y > 178 && rot.y < 182 || rot.y > 358 || rot.y < 2) {
+        //    rot.y = rot.y - 3;
+        //}
+
+        if(Input.GetAxisRaw("Horizontal") != 0) {
+            transform.Rotate(new Vector3(0, rot.y + Input.GetAxisRaw("Horizontal"), 0).normalized * 2 * speed);
+        }
+
+
+        //    rot *= Quaternion.Euler(0, (Input.GetAxisRaw("Horizontal") * speed * Time.fixedDeltaTime), 0);
+        //}
+    }
+}
