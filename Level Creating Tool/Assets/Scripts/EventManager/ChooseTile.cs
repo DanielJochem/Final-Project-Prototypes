c86cd2143eaa3e72423fbc82c05dd43daec21587
DanielJochem <daniel.jochem@gmail.com>
2016-10-09 22:41:17 +1000

    Added functionality to the three input fields (X Amt, Y Amt, Level name). On clicking the "Save" button, an error-checking method is run that checks if all three input fields are properly filled out. (I am very proud of this, works flawlessly). Also updated the UI Tile selection via keybindings to check if any of the three input fields are in focus, because we don't want to be switching around the selected tile if we are typing in those fields. Eww.

--- Level Creating Tool/Assets/Scripts/EventManager/ChooseTile.cs
+++ Level Creating Tool/Assets/Scripts/EventManager/ChooseTile.cs
@@ -8,7 +8,11 @@ public class ChooseTile : MonoBehaviour {
     public Sprite placeSprite;
     public TilePlacer tilePlacer;
 
-	void Awake () {
+    public InputField xTilesIF;
+    public InputField yTilesIF;
+    public InputField levelNameIF;
+
+    void Awake () {
         //m_keyText = GetComponent<Text>();
         EventManager.AddListener<KeyPressedEvent>(OnKeyPressed);
 	}
@@ -18,55 +22,57 @@ public class ChooseTile : MonoBehaviour {
     }
 
     void OnKeyPressed(KeyPressedEvent a_event) {
-        switch(a_event.PressedKeyCode.ToString()) {
-            case "Alpha1":
-                UITiles[0].GetComponent<Image>().sprite = placeSprite;
-                tilePlacer.selectedTile = UITiles[0].transform.GetChild(0).gameObject;
-                break;
-            case "Alpha2":
-                UITiles[1].GetComponent<Image>().sprite = placeSprite;
-                tilePlacer.selectedTile = UITiles[1].transform.GetChild(0).gameObject;
-                break;
-            case "Alpha3":
-                UITiles[2].GetComponent<Image>().sprite = placeSprite;
-                tilePlacer.selectedTile = UITiles[2].transform.GetChild(0).gameObject;
-                break;
-            case "Alpha4":
-                UITiles[3].GetComponent<Image>().sprite = placeSprite;
-                tilePlacer.selectedTile = UITiles[3].transform.GetChild(0).gameObject;
-                break;
-            case "Alpha5":
-                UITiles[4].GetComponent<Image>().sprite = placeSprite;
-                tilePlacer.selectedTile = UITiles[4].transform.GetChild(0).gameObject;
-                break;
-            case "Alpha6":
-                UITiles[5].GetComponent<Image>().sprite = placeSprite;
-                tilePlacer.selectedTile = UITiles[5].transform.GetChild(0).gameObject;
-                break;
-            case "Alpha7":
-                UITiles[6].GetComponent<Image>().sprite = placeSprite;
-                tilePlacer.selectedTile = UITiles[6].transform.GetChild(0).gameObject;
-                break;
-            case "Alpha8":
-                UITiles[7].GetComponent<Image>().sprite = placeSprite;
-                tilePlacer.selectedTile = UITiles[7].transform.GetChild(0).gameObject;
-                break;
-            case "Alpha9":
-                UITiles[8].GetComponent<Image>().sprite = placeSprite;
-                tilePlacer.selectedTile = UITiles[8].transform.GetChild(0).gameObject;
-                break;
-            case "Alpha0":
-                UITiles[9].GetComponent<Image>().sprite = placeSprite;
-                tilePlacer.selectedTile = UITiles[9].transform.GetChild(0).gameObject;
-                break;
-            case "Minus":
-                UITiles[10].GetComponent<Image>().sprite = placeSprite;
-                tilePlacer.selectedTile = UITiles[10].transform.GetChild(0).gameObject;
-                break;
-            case "Equals":
-                UITiles[11].GetComponent<Image>().sprite = placeSprite;
-                tilePlacer.selectedTile = UITiles[11].transform.GetChild(0).gameObject;
-                break;
+        if(!xTilesIF.isFocused && !yTilesIF.isFocused && !levelNameIF.isFocused) {
+            switch(a_event.PressedKeyCode.ToString()) {
+                case "Alpha1":
+                    UITiles[0].GetComponent<Image>().sprite = placeSprite;
+                    tilePlacer.selectedTile = UITiles[0].transform.GetChild(0).gameObject;
+                    break;
+                case "Alpha2":
+                    UITiles[1].GetComponent<Image>().sprite = placeSprite;
+                    tilePlacer.selectedTile = UITiles[1].transform.GetChild(0).gameObject;
+                    break;
+                case "Alpha3":
+                    UITiles[2].GetComponent<Image>().sprite = placeSprite;
+                    tilePlacer.selectedTile = UITiles[2].transform.GetChild(0).gameObject;
+                    break;
+                case "Alpha4":
+                    UITiles[3].GetComponent<Image>().sprite = placeSprite;
+                    tilePlacer.selectedTile = UITiles[3].transform.GetChild(0).gameObject;
+                    break;
+                case "Alpha5":
+                    UITiles[4].GetComponent<Image>().sprite = placeSprite;
+                    tilePlacer.selectedTile = UITiles[4].transform.GetChild(0).gameObject;
+                    break;
+                case "Alpha6":
+                    UITiles[5].GetComponent<Image>().sprite = placeSprite;
+                    tilePlacer.selectedTile = UITiles[5].transform.GetChild(0).gameObject;
+                    break;
+                case "Alpha7":
+                    UITiles[6].GetComponent<Image>().sprite = placeSprite;
+                    tilePlacer.selectedTile = UITiles[6].transform.GetChild(0).gameObject;
+                    break;
+                case "Alpha8":
+                    UITiles[7].GetComponent<Image>().sprite = placeSprite;
+                    tilePlacer.selectedTile = UITiles[7].transform.GetChild(0).gameObject;
+                    break;
+                case "Alpha9":
+                    UITiles[8].GetComponent<Image>().sprite = placeSprite;
+                    tilePlacer.selectedTile = UITiles[8].transform.GetChild(0).gameObject;
+                    break;
+                case "Alpha0":
+                    UITiles[9].GetComponent<Image>().sprite = placeSprite;
+                    tilePlacer.selectedTile = UITiles[9].transform.GetChild(0).gameObject;
+                    break;
+                case "Minus":
+                    UITiles[10].GetComponent<Image>().sprite = placeSprite;
+                    tilePlacer.selectedTile = UITiles[10].transform.GetChild(0).gameObject;
+                    break;
+                case "Equals":
+                    UITiles[11].GetComponent<Image>().sprite = placeSprite;
+                    tilePlacer.selectedTile = UITiles[11].transform.GetChild(0).gameObject;
+                    break;
+            }
         }
     }
 }
--- Level Creating Tool/Assets/Scripts/TilePlacer.cs
+++ Level Creating Tool/Assets/Scripts/TilePlacer.cs
@@ -1,18 +1,94 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TilePlacer : MonoBehaviour {
 
     public GameObject selectedTile;
+    private bool levelLoaded;
 
-	//void Start () {
-		
-	//}
+    public InputField xTilesIF;
+    public InputField yTilesIF;
+    public InputField levelNameIF;
 
+    public List<string> saveErrorArray;
+
+    public int xTiles, yTiles;
+    public string levelName;
+
+    void Awake() {
+        xTiles = yTiles = 0;
+    }
+    
 	void Update () {
         if(selectedTile != null) {
 
         }
+
+        if(!levelLoaded) {
+            if(xTiles > 0 && yTiles > 0) {
+
+                levelLoaded = true;
+            }
+        }
 	}
+
+    public void SetXTiles(InputField inputField) {
+        xTiles = int.Parse(inputField.text);
+    }
+
+    public void SetYTiles(InputField inputField) {
+        yTiles = int.Parse(inputField.text);
+    }
+
+    public void SetLevelName(InputField inputField) {
+        levelName = inputField.text;
+    }
+
+    public void TestForErrorOnSave() {
+        //if there are places that havent been filled with void
+
+        if(levelNameIF.text.Length == 0) {
+            saveErrorArray.Add("there is no input for Level Name");
+        }
+
+        if(xTilesIF.text.Length == 0 && yTilesIF.text.Length == 0) {
+            saveErrorArray.Add(saveErrorArray.Count > 0 ? "and there are no inputs for both X Amount and Y Amount" : "there are no inputs for both X Amount and Y Amount");
+
+        } else if(int.Parse(xTilesIF.text) <= 0 && int.Parse(yTilesIF.text) <= 0) {
+            saveErrorArray.Add("both X Amount and Y Amount are less than or equal to 0");
+
+        } else {
+            if(xTilesIF.text.Length == 0) {
+                saveErrorArray.Add(saveErrorArray.Count > 0 ? "or" : "there is no input for");
+                saveErrorArray.Add("X Amount");
+
+            } else if(int.Parse(xTilesIF.text) <= 0) {
+                if(saveErrorArray.Count > 0)
+                    saveErrorArray.Add("and");
+
+                saveErrorArray.Add("X Amount is less than or equal to 0");
+            }
+
+            if(yTilesIF.text.Length == 0) {
+                saveErrorArray.Add(int.Parse(xTilesIF.text) <= 0 ? "and there is no input for" : saveErrorArray.Count > 0 ? "or" : "there is no input for");
+                saveErrorArray.Add("Y Amount");
+
+            } else if(int.Parse(yTilesIF.text) <= 0) {
+                if(saveErrorArray.Count > 0)
+                    saveErrorArray.Add("and");
+
+                saveErrorArray.Add("Y Amount is less than or equal to 0");
+            }
+        }
+
+        if(saveErrorArray.Count > 0) {
+            string joined = string.Join(" ", saveErrorArray.ToArray());
+            Debug.Log("Can not save because " + joined + ".");
+            saveErrorArray.Clear();
+        } else {
+            Debug.Log("Saved Successfully!");
+        }
+    }
 }
