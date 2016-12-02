20b6327fa0b6082240a4eaf463707cde2704a372
DanielJochem <daniel.jochem@gmail.com>
2016-12-03 00:57:03 +1000

    This was soooooo much easier with 2-directional Enemy movement :P There is a bug where the Enemies will only want to move in the top 5 directions (Left, UpLeft, Up, UpRight, Right), and never choose the bottom 3 directions (DownRight, Down, DownLeft) :/

--- Final Project Game/Assets/Scripts/Enemy/EnemyMovement.cs
+++ Final Project Game/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -1,58 +1,271 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class EnemyMovement : MonoBehaviour {
-    public TurnHandler turnHandler;
+    [SerializeField]
+    private TurnHandler turnHandler;
 
+    [SerializeField]
+    private PlayerMovement player;
+    
     [HideInInspector]
     public int xTilesAmount, yTilesAmount;
 
+    [Space(20)]
     public int currentTileNumber;
 
+    private List<string> illegalMoves, acceptableMoves;
+
     [SerializeField]
-    private string lastDirection = "";
+    private List<string> directions;
 
 
     void Start() {
         turnHandler = FindObjectOfType<TurnHandler>();
+        player = FindObjectOfType<PlayerMovement>();
     }
 
 
     public void EnemyMovementLogic() {
-        if(lastDirection == "") {
-            //Debug.Log("Enemy Movement activated");
-            if(!((currentTileNumber) % xTilesAmount == 0) && ((Mathf.CeilToInt((currentTileNumber - 1) / xTilesAmount)) == (Mathf.CeilToInt(currentTileNumber / xTilesAmount)))) {
-                lastDirection = "Left";
-            } else {
-                lastDirection = "Right";
+        PlayerAndEnemyLocationChecker();
+        CheckBoundaries();
+
+        //All checks are done, now the Enemy can move!
+        MovementCheckerAndMove();
+    }
+
+
+    void PlayerAndEnemyLocationChecker() {
+        //Up
+        //If Player is not in wantedPosition
+        if(player.currentTileNumber != currentTileNumber - xTilesAmount) {
+            //If no Enemy is in that position
+            foreach(GameObject enemy in turnHandler.enemyList) {
+                if(enemy.GetComponent<EnemyMovement>().currentTileNumber == currentTileNumber - xTilesAmount) {
+                    illegalMoves.Add("Up");
+                }
+            }
+        } else {
+            illegalMoves.Add("Up");
+        }
+
+        //UpRight
+        //If Player is not in wantedPosition
+        if(player.currentTileNumber != currentTileNumber - (xTilesAmount - 1)) {
+            //If no Enemy is in that position
+            foreach(GameObject enemy in turnHandler.enemyList) {
+                if(enemy.GetComponent<EnemyMovement>().currentTileNumber == currentTileNumber - (xTilesAmount - 1)) {
+                    illegalMoves.Add("UpRight");
+                }
+            }
+        } else {
+            illegalMoves.Add("UpRight");
+        }
+
+        //Right
+        //If Player is not in wantedPosition
+        if(player.currentTileNumber != currentTileNumber + 1) {
+            //If no Enemy is in that position
+            foreach(GameObject enemy in turnHandler.enemyList) {
+                if(enemy.GetComponent<EnemyMovement>().currentTileNumber == currentTileNumber + 1) {
+                    illegalMoves.Add("Right");
+                }
+            }
+        } else {
+            illegalMoves.Add("Right");
+        }
+
+        //DownRight
+        //If Player is not in wantedPosition
+        if(player.currentTileNumber != currentTileNumber + (xTilesAmount + 1)) {
+            //If no Enemy is in that position
+            foreach(GameObject enemy in turnHandler.enemyList) {
+                if(enemy.GetComponent<EnemyMovement>().currentTileNumber == currentTileNumber + (xTilesAmount + 1)) {
+                    illegalMoves.Add("DownRight");
+                }
+            }
+        } else {
+            illegalMoves.Add("DownRight");
+        }
+
+        //Down
+        //If Player is not in wantedPosition
+        if(player.currentTileNumber != currentTileNumber + xTilesAmount) {
+            //If no Enemy is in that position
+            foreach(GameObject enemy in turnHandler.enemyList) {
+                if(enemy.GetComponent<EnemyMovement>().currentTileNumber == currentTileNumber + xTilesAmount) {
+                    illegalMoves.Add("Down");
+                }
+            }
+        } else {
+            illegalMoves.Add("Down");
+        }
+
+        //DownLeft
+        //If Player is not in wantedPosition
+        if(player.currentTileNumber != currentTileNumber + (xTilesAmount - 1)) {
+            //If no Enemy is in that position
+            foreach(GameObject enemy in turnHandler.enemyList) {
+                if(enemy.GetComponent<EnemyMovement>().currentTileNumber == currentTileNumber + (xTilesAmount - 1)) {
+                    illegalMoves.Add("DownLeft");
+                }
+            }
+        } else {
+            illegalMoves.Add("DownLeft");
+        }
+
+        //Left
+        //If Player is not in wantedPosition
+        if(player.currentTileNumber != currentTileNumber - 1) {
+            //If no Enemy is in that position
+            foreach(GameObject enemy in turnHandler.enemyList) {
+                if(enemy.GetComponent<EnemyMovement>().currentTileNumber == currentTileNumber - 1) {
+                    illegalMoves.Add("Left");
+                }
+            }
+        } else {
+            illegalMoves.Add("Left");
+        }
+
+        //UpLeft
+        //If Player is not in wantedPosition
+        if(player.currentTileNumber != currentTileNumber - (xTilesAmount + 1)) {
+            //If no Enemy is in that position
+            foreach(GameObject enemy in turnHandler.enemyList) {
+                if(enemy.GetComponent<EnemyMovement>().currentTileNumber == currentTileNumber - (xTilesAmount + 1)) {
+                    illegalMoves.Add("UpLeft");
+                }
+            }
+        } else {
+            illegalMoves.Add("UpLeft");
+        }
+    }
+
+
+    void CheckBoundaries() {
+        //Up
+        if(currentTileNumber - xTilesAmount <= 0) {
+            illegalMoves.Add("Up");
+        }
+
+        //UpRight
+        if(currentTileNumber - (xTilesAmount - 1) < 2) {
+            illegalMoves.Add("UpRight");
+        }
+
+        //Right
+        if(currentTileNumber % xTilesAmount == 0) {
+            illegalMoves.Add("Right");
+        }
+
+        //DownRight
+        if(currentTileNumber + (xTilesAmount + 1) > xTilesAmount * yTilesAmount) {
+            illegalMoves.Add("DownRight");
+        }
+
+        //Down
+        if(currentTileNumber + xTilesAmount > xTilesAmount * yTilesAmount) {
+            illegalMoves.Add("Down");
+        }
+
+        //DownLeft
+        if(currentTileNumber + (xTilesAmount - 1) > (xTilesAmount * yTilesAmount) - 1) {
+            illegalMoves.Add("DownLeft");
+        }
+
+        //Left
+        if(currentTileNumber % xTilesAmount == 1) {
+            illegalMoves.Add("Left");
+        }
+
+        //UpLeft
+        if(currentTileNumber - (xTilesAmount + 1) <= 0) {
+            illegalMoves.Add("UpLeft");
+        }
+    }
+
+
+    string RandomDirection() {
+        int directionChoice = Random.Range(0, acceptableMoves.Count);
+        return acceptableMoves[directionChoice];
+    }
+
+
+    void MovementCheckerAndMove() {
+        string moveDirection = "";
+
+        foreach(string directionToCheck in directions) {
+            bool directionFound = false;
+            foreach(string badDirection in illegalMoves) {
+                if(badDirection == directionToCheck) {
+                    directionFound = true;
+                    break;
+                }
+            }
+
+            if(!directionFound) {
+                acceptableMoves.Add(directionToCheck);
             }
         }
 
-        //Checking for the 4 corners, they are hell, we never want enemies to be in them.
-        if(lastDirection == "Left") {
-            if(currentTileNumber + 1 == xTilesAmount || currentTileNumber + 1 == xTilesAmount * yTilesAmount) {
-                lastDirection = "Right";
-            }
-        } else if(lastDirection == "Right") {
-            if(currentTileNumber - 1 == 1 || currentTileNumber - 1 == ((xTilesAmount * yTilesAmount) - (xTilesAmount - 1))) {
-                lastDirection = "Left";
-            }
+        if(acceptableMoves.Count > 0) {
+            moveDirection = RandomDirection();
         }
 
-        //Need to check if player or any other enemies are in wanted tile /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        //if()
-        if(lastDirection == "Left") {
-            //Move Right
-            gameObject.transform.position += new Vector3(1, 0, 0);
-            currentTileNumber += 1;
-            lastDirection = "Right";
-        } else if(lastDirection == "Right") {
-            //Move Left
-            gameObject.transform.position += new Vector3(-1, 0, 0);
-            currentTileNumber -= 1;
-            lastDirection = "Left";
+        if(moveDirection != "") {
+            Move(moveDirection);
         }
 
+        illegalMoves.Clear();
+        acceptableMoves.Clear();
+    }
+
+
+    void Move(string direction) {
         //Debug.Log("Enemy Moved");
+
+        switch(direction) {
+            case "Up":
+                //Move Up
+                gameObject.transform.position += new Vector3(0, 1, 0);
+                currentTileNumber -= xTilesAmount;
+                break;
+            case "UpRight":
+                //Move UpRight
+                gameObject.transform.position += new Vector3(1, 1, 0);
+                currentTileNumber -= (xTilesAmount - 1);
+                break;
+            case "Right":
+                //Move Right
+                gameObject.transform.position += new Vector3(1, 0, 0);
+                currentTileNumber += 1;
+                break;
+            case "DownRight":
+                //Move DownRight
+                gameObject.transform.position += new Vector3(1, -1, 0);
+                currentTileNumber += (xTilesAmount + 1);
+                break;
+            case "Down":
+                //Move Down
+                gameObject.transform.position += new Vector3(0, -1, 0);
+                currentTileNumber += xTilesAmount;
+                break;
+            case "DownLeft":
+                //Move DownLeft
+                gameObject.transform.position += new Vector3(-1, -1, 0);
+                currentTileNumber += (xTilesAmount - 1);
+                break;
+            case "Left":
+                //Move Left
+                gameObject.transform.position += new Vector3(-1, 0, 0);
+                currentTileNumber -= 1;
+                break;
+            case "UpLeft":
+                //Move UpLeft
+                gameObject.transform.position += new Vector3(-1, 1, 0);
+                currentTileNumber -= (xTilesAmount + 1);
+                break;
+        }
     }
 }
