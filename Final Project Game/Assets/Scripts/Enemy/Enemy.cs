52a22f8bf1ff4b6a8595bbe818b123e7ad8c4343
DanielJochem <daniel.jochem@gmail.com>
2017-02-01 23:18:12 +1000

    You can now type in which tiles relative to the current tile of the enemy is enemy attackable. Some cool custom inspector magic was added so that the previous statement can be done with simplicity and ease of readability. So far it prints to console what tiles an enemy can attack on via the tile number. Checked more references into Enemy scripts, may clean up in the future for references work, maybe with some object script inheritance, but for now it is fine.

--- Final Project Game/Assets/Scripts/Enemy/Enemy.cs
+++ Final Project Game/Assets/Scripts/Enemy/Enemy.cs
@@ -1,16 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Enemy : MonoBehaviour {
-    private EnemyAttack attack;
-    private TurnHandler turnHandler;
+
+    [HideInInspector]
+    public EnemyAttack attack;
+    [HideInInspector]
+    public EnemyMovement movement;
+    [HideInInspector]
+    public TurnHandler turnHandler;
+
+    [HideInInspector]
+    public List<GameObject> gridOfTiles;
+
+    [HideInInspector]
+    public int xTilesAmount, yTilesAmount;
 
 
     void Start() {
         turnHandler = FindObjectOfType<TurnHandler>();
         attack = FindObjectOfType<EnemyAttack>();
+        movement = FindObjectOfType<EnemyMovement>();
     }
-    
+
 
     public void Attack() {
         attack.EnemyAttackLogic();
